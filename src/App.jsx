edd0d0b70ddbe54bc0565f542ce5d70a7960c839
BartCatalano <catalanobartolomeo@gmail.com>
2025-01-08 15:23:55 +0100

    feat: struttura card fatta

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,33 @@ import axios from "axios";
 function App() {
 
   const loadData = () => {
-axios.get("https://api.api-onepiece.com/v2/characters/en/crew/1").then((resp) => {
-  console.log(resp);
-  
-
-})
-
+    axios.get("https://api.api-onepiece.com/v2/characters/en/crew/1").then((resp) => {
+      console.log(resp);
+    })
   };
-  
+
   return (
     <>
       <div className="container mt-20">
-        <button onClick={loadData}>Clicca</button>
+        <button onClick={loadData}>Ciurma di Cappello di Paglia</button>
       </div>
+      <section className="container">
+        <div className="row">
+          <div className="col">
+            <div className="card row">
+              <div className="col">
+              <div>nome</div>
+              <div>ciurma</div>
+              <div>ruolo</div>
+              <div>taglia</div>
+              <div>haky e frutto</div>
+              </div>
+              <div className="col">immagine</div>
+              
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   )
 }
