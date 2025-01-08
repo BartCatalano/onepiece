c860d171227f8d2e0bed1e285c43e6a9b1a7b98a
BartCatalano <catalanobartolomeo@gmail.com>
2025-01-08 23:04:59 +0100

    feat: grafica meno brutta <3

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
   const loadData = () => {
     axios.get("https://api.api-onepiece.com/v2/characters/en/crew/1").then((resp) => {
-      console.log(resp);
+      console.log(resp.data);
       setCharacters(resp.data);
     })
   };
@@ -21,22 +21,23 @@ function App() {
       <section className="container">
 
         <div className="row">
-          {characters.map((curItem) => (<div key={curItem.id}>
-
+          {characters.map((curItem) => (
+            <div key={curItem.id}>
             <div className="col">
               <div className="card">
                 <div className="cardName">{curItem.name}</div>
-                <div className="cardInfo">Bounty:<span className="cardValue"> {curItem.bounty}</span></div>
-                <div className="cardInfo">Job: <span className="cardValue">{curItem.job}</span></div>
-                <div className="cardInfo">Age: <span className="cardValue">{curItem.age}</span> </div>
+                <div className="cardInfo">
+                <div className="mtb-10">Bounty:<span className="cardValue"> {curItem.bounty}</span></div>
+                <div className="mtb-10">Job: <span className="cardValue">{curItem.job}</span></div>
+                <div className="mtb-10">Age: <span className="cardValue">{curItem.age}</span> </div>
+                </div>
+                <div className="cardInfo"></div>
               </div>
             </div>
           </div>
 
           ))}
         </div>
-
-
       </section>
     </>
   )
