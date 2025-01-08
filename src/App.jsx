0b8f507ba98d0c119e6327872c712ab79c66c855
BartCatalano <catalanobartolomeo@gmail.com>
2025-01-08 16:10:28 +0100

    feat: map per card fatto. style(WIP)

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,42 @@
 import axios from "axios";
+import { useState } from "react";
 
 function App() {
+  const data = [];
+
+  const [characters, setCharacters] = useState([]);
 
   const loadData = () => {
     axios.get("https://api.api-onepiece.com/v2/characters/en/crew/1").then((resp) => {
       console.log(resp);
+      setCharacters(resp.data);
     })
   };
 
   return (
     <>
       <div className="container mt-20">
-        <button onClick={loadData}>Ciurma di Cappello di Paglia</button>
+        <button onClick={loadData}><h2>Ciurma di Cappello di Paglia</h2></button>
       </div>
       <section className="container">
-        <div className="row">
-          <div className="col">
-            <div className="card row">
-              <div className="col">
-              <div>nome</div>
-              <div>ciurma</div>
-              <div>ruolo</div>
-              <div>taglia</div>
-              <div>haky e frutto</div>
-              </div>
-              <div className="col">immagine</div>
-              
+        
+          <div className="row">
+            {characters.map((curItem) => (<div key={curItem.id}>
+             
+            <div className="col">
+              <div className="card">
+              <div className="cardName">{curItem.name}</div>
+              <div>{curItem.bounty}</div>
+              <div>{curItem.job}</div>
+              <div>{curItem.age}</div>
             </div>
-          </div>
-        </div>
+            </div>
+            </div>
+           
+            ))}
+ </div>
+          
+        
       </section>
     </>
   )
